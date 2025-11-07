220828ec8084a8b6f49f31a82cb8623d2fb70973
mkoscheev <mkoscheev@alfabank.ru>
2025-11-07 19:58:48 +0500

    формирование информации о заказе

--- models/order.py
+++ models/order.py
@@ -1,12 +1,22 @@
 import datetime
+from decimal import Decimal
 
 from pydantic import BaseModel, Field
 
 
-class Order(BaseModel):
-    id: int = Field()
-    client_status: str = Field()
-    manager: str = Field(default="Не назначен")
-    created: datetime.datetime
+class OrderItem(BaseModel):
+    id: int
+    article_number: str | None = None
+    manufacture: str | None = None
+    name: str | None = None
+    price: Decimal | None = None
 
+
+class Order(BaseModel):
+    id: int | None = None
+    client_status: str = "Новый"
+    manager: str = "Не назначен"
+    created: datetime.datetime | None = None
+    initial_requirements: str
+    order_item_list: list[OrderItem] | None = None
 
--- utils.py
+++ utils.py
@@ -7,4 +7,9 @@ def build_vehicle_name(vehicle: Vehicle) -> str:
 
 
 def build_order_info(order: Order) -> str:
-    return f""
+    base_info = f"Номер: {order.id} от {order.created.date()}, статус: {order.client_status}"
+    if order.order_item_list is not None and len(order.order_item_list) > 0:
+        amount = sum(item.price for item in order.order_item_list if item.price is not None)
+        return f"{base_info}, количество позиций: {len(order.order_item_list)}, общая стоимость: {amount}"
+    else:
+        return base_info
